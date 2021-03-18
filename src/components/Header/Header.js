69416c5ca529458fad1bf1669034029a44709101
Kevin Holder <kevholderx@gmail.com>
2021-03-18 15:47:49 -0400

    help bubble goes tot elegram

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,7 +32,7 @@ const Header = ({ t, currentWallet }) => {
 				<WalletStatusButton onClick={() => updateCurrentPage('walletSelection', dispatch)}>
 					{shortenAddress(currentWallet)}
 				</WalletStatusButton>
-				<RoundButton as="a" href="https://help.synthetix.io/hc/en-us" target="_blank">
+				<RoundButton as="a" href="https://t.me/oikoscash" target="_blank">
 					<SupportBubble />
 				</RoundButton>
 				<LanguageButtonWrapper>
