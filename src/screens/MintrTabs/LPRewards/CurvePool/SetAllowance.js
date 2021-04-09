d16e46b0b5a0b105b7da690bbfd9bcf9688f1e67
Oikos.cash <albert@oikos.cash>
2021-04-09 13:18:36 -0400

    fix gas estimation / derive log

--- src/screens/MintrTabs/LPRewards/CurvePool/SetAllowance.js
+++ src/screens/MintrTabs/LPRewards/CurvePool/SetAllowance.js
@@ -29,7 +29,7 @@ const SetAllowance = ({ t, goBack }) => {
 		try {
 			setError(null);
 
-			const gasEstimate = await curveLPTokenContract.estimate.approve(
+			const gasEstimate = await curveLPTokenContract.estimateGas.approve(
 				curvepoolContract.address,
 				parseEther(ALLOWANCE_LIMIT.toString())
 			);
@@ -63,7 +63,7 @@ const SetAllowance = ({ t, goBack }) => {
 				<ButtonTertiary onClick={goBack}>{t('button.navigation.back')}</ButtonTertiary>
 			</Navigation>
 			<TitleContainer>
-				<Logo src="/images/iearn.svg" />
+				<Logo src="/images/derive.svg" />
 				<PageTitle>{t('curvepool.title')}</PageTitle>
 				<PLarge>{t('curvepool.locked.subtitle')}</PLarge>
 			</TitleContainer>
