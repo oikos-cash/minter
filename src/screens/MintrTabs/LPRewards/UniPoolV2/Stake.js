ea69d8d5f15b27f8b86d341e855de07765eb4b5f
Oikos.cash <albert@oikos.cash>
2021-04-28 17:01:18 -0400

    add V2 screens

---
+++ src/screens/MintrTabs/LPRewards/UniPoolV2/Stake.js
@@ -0,0 +1,247 @@
+/* eslint-disable */
+import React, { useState, useContext, useCallback, useEffect } from 'react';
+import styled from 'styled-components';
+import { withTranslation } from 'react-i18next';
+
+import snxJSConnector from '../../../../helpers/snxJSConnector';
+import { Store } from '../../../../store';
+
+import { bigNumberFormatter, formatCurrency } from '../../../../helpers/formatters';
+import TransactionPriceIndicator from '../../../../components/TransactionPriceIndicator';
+import { updateGasLimit } from '../../../../ducks/network';
+
+import { PageTitle, PLarge, ButtonTertiaryLabel } from '../../../../components/Typography';
+import DataBox from '../../../../components/DataBox';
+import { ButtonTertiary, ButtonPrimary } from '../../../../components/Button';
+
+import UnipoolActionsV2 from '../../../UnipoolActionsV2';
+
+const TRANSACTION_DETAILS = {
+	stake: {
+		contractFunction: 'stake',
+		gasLimit: 220000,
+	},
+	claim: {
+		contractFunction: 'getReward',
+		gasLimit: 200000,
+	},
+	unstake: {
+		contractFunction: 'withdraw',
+		gasLimit: 125000,
+	},
+	exit: {
+		contractFunction: 'exit',
+		gasLimit: 250000,
+	},
+};
+
+const Stake = ({ t, goBack }) => {
+	const { unipoolV2Contract } = snxJSConnector;
+	const [balances, setBalances] = useState(null);
+	const [currentScenario, setCurrentScenario] = useState({});
+	const [withdrawAmount, setWithdrawAmount] = useState('');
+	const {
+		state: {
+			wallet: { currentWallet },
+		},
+		dispatch,
+	} = useContext(Store);
+
+	const fetchData = useCallback(async () => {
+		if (!snxJSConnector.initialized) return;
+		try {
+			const { uniswapV2Contract, unipoolV2Contract } = snxJSConnector;
+			const [univ1Held, univ1Staked, rewards] = await Promise.all([
+				uniswapV2Contract.balanceOf(currentWallet),
+				unipoolV2Contract.balanceOf(currentWallet),
+				unipoolV2Contract.earned(currentWallet),
+			]);
+
+			setBalances({
+				univ1Held: bigNumberFormatter(univ1Held),
+				univ1HeldBN: univ1Held,
+				univ1Staked: bigNumberFormatter(univ1Staked),
+				univ1StakedBN: univ1Staked,
+				rewards: bigNumberFormatter(rewards),
+			});
+			updateGasLimit(TRANSACTION_DETAILS.stake.gasLimit, dispatch);
+		} catch (e) {
+			console.log(e);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentWallet, snxJSConnector.initialized]);
+
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
+
+	useEffect(() => {
+		if (!currentWallet) return;
+		const { uniswapV2Contract, unipoolV2Contract } = snxJSConnector;
+
+		unipoolV2Contract.on('Staked', user => {
+			if (user === currentWallet) {
+				fetchData();
+			}
+		});
+
+		unipoolV2Contract.on('Withdrawn', user => {
+			if (user === currentWallet) {
+				fetchData();
+			}
+		});
+
+		unipoolV2Contract.on('RewardPaid', user => {
+			if (user === currentWallet) {
+				fetchData();
+			}
+		});
+
+		return () => {
+			if (snxJSConnector.initialized) {
+				unipoolV2Contract.removeAllListeners('Staked');
+				unipoolV2Contract.removeAllListeners('Withdrawn');
+				unipoolV2Contract.removeAllListeners('RewardPaid');
+			}
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentWallet]);
+
+	return (
+		<Container>
+			<UnipoolActionsV2 {...currentScenario} onDestroy={() => setCurrentScenario({})} />
+			<Navigation>
+				<ButtonTertiary onClick={goBack}>{t('button.navigation.back')}</ButtonTertiary>
+				<ButtonTertiary
+					as="a"
+					target="_blank"
+					href={`https://bscscan.com/address/${unipoolV2Contract.address}`}
+				>
+					{t('lpRewards.shared.buttons.goToContract')} ↗
+				</ButtonTertiary>
+			</Navigation>
+			<PageTitle>{t('unipoolV2.title')}</PageTitle>
+			<PLarge>{t('unipoolV2.unlocked.subtitle')}</PLarge>
+			<PLarge>
+				<Link href="https://exchange.pancakeswap.finance/#/add/BNB/0x6BF2Be9468314281cD28A94c35f967caFd388325" target="_blank">
+					{<ButtonTertiaryLabel>{"Go to Pancake Swap"}</ButtonTertiaryLabel>}
+				</Link>
+			</PLarge>	
+		
+			<BoxRow>
+				<DataBox
+					heading={t('lpRewards.shared.data.balance')}
+					body={`${balances ? formatCurrency(balances.univ1Held) : 0} CAKE`}
+				/>
+				<DataBox
+					heading={t('lpRewards.shared.data.staked')}
+					body={`${balances ? formatCurrency(balances.univ1Staked) : 0} CAKE`}
+				/>
+				<DataBox
+					heading={t('lpRewards.shared.data.rewardsAvailable')}
+					body={`${balances ? formatCurrency(balances.rewards) : 0} OKS`}
+				/>
+			</BoxRow>
+			<ButtonBlock>
+				<ButtonRow>
+					<ButtonAction
+						disabled={!balances || !balances.univ1Held}
+						onClick={() =>
+							setCurrentScenario({
+								action: 'stake',
+								label: t('lpRewards.shared.actions.staking'),
+								amount: `${balances && formatCurrency(balances.univ1Held)} CAKE`,
+								param: balances && balances.univ1HeldBN,
+								...TRANSACTION_DETAILS['stake'],
+							})
+						}
+					>
+						{t('lpRewards.shared.buttons.stake')}
+					</ButtonAction>
+					<ButtonAction
+						disabled={!balances || !balances.rewards}
+						onClick={() =>
+							setCurrentScenario({
+								action: 'claim',
+								label: t('lpRewards.shared.actions.claiming'),
+								amount: `${balances && formatCurrency(balances.rewards)} OKS`,
+								...TRANSACTION_DETAILS['claim'],
+							})
+						}
+					>
+						{t('lpRewards.shared.buttons.claim')}
+					</ButtonAction>
+				</ButtonRow>
+				<ButtonRow>
+					<ButtonAction
+						disabled={!balances || !balances.univ1Staked}
+						onClick={() =>
+							setCurrentScenario({
+								action: 'unstake',
+								label: t('lpRewards.shared.actions.unstaking'),
+								amount: `${balances && formatCurrency(balances.univ1Staked)} CAKE`,
+								param: balances && balances.univ1StakedBN,
+								...TRANSACTION_DETAILS['unstake'],
+							})
+						}
+					>
+						{t('lpRewards.shared.buttons.unstake')}
+					</ButtonAction>
+					<ButtonAction
+						disabled={!balances || (!balances.univ1Staked && !balances.rewards)}
+						onClick={() =>
+							setCurrentScenario({
+								action: 'exit',
+								label: t('lpRewards.shared.actions.exiting'),
+								amount: `${balances && formatCurrency(balances.univ1Staked)} CAKE & ${balances &&
+									formatCurrency(balances.rewards)} OKS`,
+								...TRANSACTION_DETAILS['exit'],
+							})
+						}
+					>
+						{t('lpRewards.shared.buttons.exit')}
+					</ButtonAction>
+				</ButtonRow>
+			</ButtonBlock>
+			<TransactionPriceIndicator canEdit={true} />
+		</Container>
+	);
+};
+
+const Link = styled.a`
+	text-decoration-color: ${props => props.theme.colorStyles.buttonTertiaryText};
+`;
+
+const Container = styled.div`
+	min-height: 850px;
+`;
+
+const Navigation = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 40px;
+`;
+
+const BoxRow = styled.div`
+	margin-top: 42px;
+	display: flex;
+`;
+
+const ButtonBlock = styled.div`
+	margin-top: 58px;
+`;
+
+const ButtonRow = styled.div`
+	display: flex;
+	margin-bottom: 28px;
+`;
+
+const ButtonAction = styled(ButtonPrimary)`
+	flex: 1;
+	width: 10px;
+	&:first-child {
+		margin-right: 34px;
+	}
+`;
+
+export default withTranslation()(Stake);
