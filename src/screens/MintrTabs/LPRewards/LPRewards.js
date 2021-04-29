e07293c7060cda90931a2fabe17ca870862da3d1
Oikos.cash <albert@oikos.cash>
2021-04-29 13:05:15 -0400

    compute APR

--- src/screens/MintrTabs/LPRewards/LPRewards.js
+++ src/screens/MintrTabs/LPRewards/LPRewards.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
@@ -8,8 +8,17 @@ import CurvePool from './CurvePool';
 import UniPoolV2 from './UniPoolV2';
 import UniPoolDRV from './UniPoolDRV';
 
-import { H1, PageTitle } from '../../../components/Typography';
+import { H1, H2, PageTitle } from '../../../components/Typography';
 import PageContainer from '../../../components/PageContainer';
+
+import snxJSConnector from '../../../helpers/snxJSConnector';
+import { Store } from '../../../store';
+
+import { bytesFormatter, parseBytes32String } from '../../../helpers/formatters';
+import { parseUnits, formatUnits } from "@ethersproject/units"
+import { BigNumber } from "@ethersproject/bignumber"
+
+const bigNumberFormatter = value => Number(snxJSConnector.utils.formatEther(value));
 
 const POOLS = [
 	{
@@ -26,6 +35,18 @@ const POOLS2 = [
 	},
 	{ title: 'lpRewards.actions.derivepool.title', name: 'pancakeDRV' },	
 ];
+
+const formatBNToString= (bn, nativePrecison, decimalPlaces) => {
+ 
+	const fullPrecision = formatUnits(bn, nativePrecison)
+	const decimalIdx = fullPrecision.indexOf(".")
+	return decimalPlaces === undefined || decimalIdx === -1
+	  ? fullPrecision
+	  : fullPrecision.slice(
+		  0,
+		  decimalIdx + (decimalPlaces > 0 ? decimalPlaces + 1 : 0), // don't include decimal point if places = 0
+		)
+  }
 
 const LPRewards = () => {
 	const { t } = useTranslation();
@@ -45,6 +66,59 @@ const LPRewards = () => {
 		}
 	};
 
+	const { curveLPTokenContract, deriveOUSDContract } = snxJSConnector;
+	const {
+		state: {
+			wallet: { currentWallet },
+		},
+		dispatch,
+	} = useContext(Store);
+
+	//const [lpTokenBalance, setLPTokenBalance] = useState(0)
+	//const [totalLpTokenBalance, setTotalLpTokenBalance] = useState(0)
+	const [oikosAPR, setOikosAPR] = useState(0)
+
+	useEffect(() => {
+		async function getData() {
+			const [userLpTokenBalance, totalLpTokenBalance] = await Promise.all([
+				curveLPTokenContract.balanceOf(currentWallet || "0x0"),
+				curveLPTokenContract.totalSupply(),
+		])
+		const synthsP = snxJSConnector.snxJS.ExchangeRates.ratesForCurrencies(
+			['OKS', 'oUSD', 'oBNB'].map(bytesFormatter)
+		);
+		const [synths] = await Promise.all([synthsP]);
+		const [oks, ousd, sbnb] = synths.map(bigNumberFormatter);
+
+	     // (weeksPerYear * OIKOSPerWeek * OIKOSPrice) / (LPTokenPrice * totalLPTokenBalance)
+		 const oikosAPRNumerator = BigNumber.from((13 * 240000) + 5000000)
+		 .mul(BigNumber.from(10).pow(18))
+		 .mul(parseUnits(String(oks || 0), 18))
+
+		 const oikosAPRDenominator = totalLpTokenBalance
+		 .mul(
+		   parseUnits(
+			 String(1 || 0),
+			 6,
+		   ),
+		 )
+		 .div(1e6)
+  
+ 
+	   const _oikosApr = totalLpTokenBalance.isZero()
+		 ? oikosAPRNumerator
+		 : oikosAPRNumerator.div(oikosAPRDenominator)
+		 
+ 
+		setOikosAPR((Number(formatBNToString(_oikosApr)) * 100).toFixed(2))
+		//setLPTokenBalance(userLpTokenBalance)
+		//setTotalLpTokenBalance(totalLpTokenBalance)
+		}
+	
+		getData()
+	  }, [])
+
+
 	return (
 		<PageContainer>
 			{currentPool ? (
@@ -53,12 +127,25 @@ const LPRewards = () => {
 				<>
 					<PageTitle>{t('lpRewards.intro.title')}</PageTitle>
 					<ButtonRow>
-						{POOLS.map(({ title, name }) => {
+						{POOLS.map(({ title, name }, idx) => {
+							let apr, subtitle, link
+							if (name == "derive") {
+								apr = oikosAPR
+								subtitle = "derive.fi"
+								link = subtitle 
+							} else if (name == "pancake") {
+								apr = 0
+								subtitle = "(Discountinued)"
+								link = "v1exchange.pancakeswap.finance/#/remove/BNB/0x6BF2Be9468314281cD28A94c35f967caFd388325"
+							}
+							
 							return (
-								<Button onClick={() => setCurrentPool(name)}>
+								<Button key={idx} onClick={() => setCurrentPool(name)}>
 									<ButtonContainer>
 										<ActionImage src={`/images/${name}.png`} big />
 										<H1>{t(title)}</H1>
+										<H2><a href={`https://${link}`} target="_blank" rel="noreferrer">{subtitle}</a></H2>
+										<H2>APR: {apr} %</H2>
 									</ButtonContainer>
 								</Button>
 							);
@@ -66,9 +153,9 @@ const LPRewards = () => {
 					</ButtonRow>
 					<br />
 					<ButtonRow>
-						{POOLS2.map(({ title, name }) => {
+						{POOLS2.map(({ title, name }, idx) => {
 							return (
-								<Button onClick={() => setCurrentPool(name)}>
+								<Button key={idx} onClick={() => setCurrentPool(name)}>
 									<ButtonContainer>
 										<ActionImage src={`/images/${name}.png`} big />
 										<H1>{t(title)}</H1>
