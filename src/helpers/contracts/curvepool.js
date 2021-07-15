0e4aac68916e5bb4728b7bfccb3d462b11d6f810
Albert Rodriguez <albert@oikos.cash>
2021-07-15 17:15:41 -0400

    amend address

--- src/helpers/contracts/curvepool.js
+++ src/helpers/contracts/curvepool.js
@@ -1,264 +1,539 @@
 export default {
-	oldAddress: '0xED97E2B7Fb473E4a589CeB379CCAB8ee53Abac49',
-	address: '0x8517c11e0459eec933391476eae41613e9e52a1d',
+	oldAddress: '0x8517c11e0459eec933391476eae41613e9e52a1d',
+	address: '0x6be8a4C0C4FF684E2e1Fc1F645d6d4f2a81fE00c',
 	abi: [
 		{
-			anonymous: false,
-			inputs: [
-				{ indexed: true, internalType: 'address', name: 'previousOwner', type: 'address' },
-				{ indexed: true, internalType: 'address', name: 'newOwner', type: 'address' },
-			],
-			name: 'OwnershipTransferred',
-			type: 'event',
+		  "constant": true,
+		  "inputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "name": "rewards",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			anonymous: false,
-			inputs: [{ indexed: false, internalType: 'uint256', name: 'reward', type: 'uint256' }],
-			name: 'RewardAdded',
-			type: 'event',
+		  "constant": false,
+		  "inputs": [
+			{
+			  "name": "_rewardDistribution",
+			  "type": "address"
+			}
+		  ],
+		  "name": "setRewardDistribution",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
 		},
 		{
-			anonymous: false,
-			inputs: [
-				{ indexed: true, internalType: 'address', name: 'user', type: 'address' },
-				{ indexed: false, internalType: 'uint256', name: 'reward', type: 'uint256' },
-			],
-			name: 'RewardPaid',
-			type: 'event',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "rewardDistribution",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			anonymous: false,
-			inputs: [
-				{ indexed: true, internalType: 'address', name: 'user', type: 'address' },
-				{ indexed: false, internalType: 'uint256', name: 'amount', type: 'uint256' },
-			],
-			name: 'Staked',
-			type: 'event',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "totalSupply",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			anonymous: false,
-			inputs: [
-				{ indexed: true, internalType: 'address', name: 'user', type: 'address' },
-				{ indexed: false, internalType: 'uint256', name: 'amount', type: 'uint256' },
-			],
-			name: 'Withdrawn',
-			type: 'event',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "DURATION",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'DURATION',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "gift",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
-			name: 'balanceOf',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "underlying",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
-			name: 'earned',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [
+			{
+			  "name": "account",
+			  "type": "address"
+			}
+		  ],
+		  "name": "balanceOf",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: false,
-			inputs: [],
-			name: 'exit',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "constant": false,
+		  "inputs": [],
+		  "name": "renounceOwnership",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
 		},
 		{
-			constant: false,
-			inputs: [],
-			name: 'getReward',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "rewardRate",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'isOwner',
-			outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "oks",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'lastTimeRewardApplicable',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "name": "userRewardPerTokenPaid",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'lastUpdateTime',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "owner",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: false,
-			inputs: [{ internalType: 'uint256', name: 'reward', type: 'uint256' }],
-			name: 'notifyRewardAmount',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "isOwner",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "bool"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'owner',
-			outputs: [{ internalType: 'address', name: '', type: 'address' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "drv",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "address"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'periodFinish',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "lastUpdateTime",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: false,
-			inputs: [],
-			name: 'renounceOwnership',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "rewardPerTokenStored",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'rewardPerToken',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "periodFinish",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'rewardPerTokenStored',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": false,
+		  "inputs": [
+			{
+			  "name": "newOwner",
+			  "type": "address"
+			}
+		  ],
+		  "name": "transferOwnership",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'rewardRate',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "inputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "constructor"
 		},
 		{
-			constant: true,
-			inputs: [{ internalType: 'address', name: '', type: 'address' }],
-			name: 'rewards',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "anonymous": false,
+		  "inputs": [
+			{
+			  "indexed": false,
+			  "name": "reward",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "RewardAdded",
+		  "type": "event"
 		},
 		{
-			constant: false,
-			inputs: [{ internalType: 'address', name: '_rewardDistribution', type: 'address' }],
-			name: 'setRewardDistribution',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "anonymous": false,
+		  "inputs": [
+			{
+			  "indexed": true,
+			  "name": "user",
+			  "type": "address"
+			},
+			{
+			  "indexed": false,
+			  "name": "amount",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "Staked",
+		  "type": "event"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'snx',
-			outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "anonymous": false,
+		  "inputs": [
+			{
+			  "indexed": true,
+			  "name": "user",
+			  "type": "address"
+			},
+			{
+			  "indexed": false,
+			  "name": "amount",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "Withdrawn",
+		  "type": "event"
 		},
 		{
-			constant: false,
-			inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
-			name: 'stake',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "anonymous": false,
+		  "inputs": [
+			{
+			  "indexed": true,
+			  "name": "user",
+			  "type": "address"
+			},
+			{
+			  "indexed": false,
+			  "name": "reward",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "RewardPaid",
+		  "type": "event"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'totalSupply',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "anonymous": false,
+		  "inputs": [
+			{
+			  "indexed": true,
+			  "name": "previousOwner",
+			  "type": "address"
+			},
+			{
+			  "indexed": true,
+			  "name": "newOwner",
+			  "type": "address"
+			}
+		  ],
+		  "name": "OwnershipTransferred",
+		  "type": "event"
 		},
 		{
-			constant: false,
-			inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
-			name: 'transferOwnership',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "anonymous": false,
+		  "inputs": [
+			{
+			  "indexed": true,
+			  "name": "from",
+			  "type": "address"
+			},
+			{
+			  "indexed": true,
+			  "name": "to",
+			  "type": "address"
+			},
+			{
+			  "indexed": false,
+			  "name": "value",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "Transfer",
+		  "type": "event"
 		},
 		{
-			constant: true,
-			inputs: [],
-			name: 'uni',
-			outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "name",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "string"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: true,
-			inputs: [{ internalType: 'address', name: '', type: 'address' }],
-			name: 'userRewardPerTokenPaid',
-			outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-			payable: false,
-			stateMutability: 'view',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "symbol",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "string"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
 		{
-			constant: false,
-			inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
-			name: 'withdraw',
-			outputs: [],
-			payable: false,
-			stateMutability: 'nonpayable',
-			type: 'function',
+		  "constant": true,
+		  "inputs": [],
+		  "name": "decimals",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint8"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
 		},
-	],
+		{
+		  "constant": true,
+		  "inputs": [],
+		  "name": "lastTimeRewardApplicable",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [],
+		  "name": "rewardPerToken",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
+		},
+		{
+		  "constant": true,
+		  "inputs": [
+			{
+			  "name": "account",
+			  "type": "address"
+			}
+		  ],
+		  "name": "earned",
+		  "outputs": [
+			{
+			  "name": "",
+			  "type": "uint256"
+			}
+		  ],
+		  "payable": false,
+		  "stateMutability": "view",
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [
+			{
+			  "name": "amount",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "stake",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [
+			{
+			  "name": "amount",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "withdraw",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [],
+		  "name": "exit",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [],
+		  "name": "getReward",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
+		},
+		{
+		  "constant": false,
+		  "inputs": [
+			{
+			  "name": "reward",
+			  "type": "uint256"
+			}
+		  ],
+		  "name": "notifyRewardAmount",
+		  "outputs": [],
+		  "payable": false,
+		  "stateMutability": "nonpayable",
+		  "type": "function"
+		}
+	  ],
 };
